3d91b34a062d3a10e32277bdcb0376f08846dbbf
Carlos Orrego <orregoc@globalhitss.com>
2026-01-25 21:18:15 -0300

    add more kotlin functions

--- app/src/main/java/com/example/s1_minutanutricional/ui/screens/MenuSemanalScreen.kt
+++ app/src/main/java/com/example/s1_minutanutricional/ui/screens/MenuSemanalScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.s1_minutanutricional.R
 import com.example.s1_minutanutricional.ui.components.BotonPrincipal
